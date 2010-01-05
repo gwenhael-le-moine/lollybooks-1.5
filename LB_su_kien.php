977ea67a18338e3b303a2b7949694cbe6287685e
Gwenhael Le Moine <cycojesus@darkstar.example.net>
2010-01-05 17:58:26 +0700

    cleaning
    Signed-off-by: Gwenhael Le Moine <cycojesus@darkstar.example.net>

--- LB_su_kien.php
+++ LB_su_kien.php
@@ -9,13 +9,14 @@
 
 <div id="container" class="page">
   <div id="content">
-    <div class="entry-content su-kien">
+
       <div id="main-event">
+
         <?php
           $category_posts=get_posts( array('category_name'=>'event', 'numberposts'=>'1') );
           foreach($category_posts as $post) {
         ?>
-        <div id="post-<?php the_ID() ?>" class="">
+        <div id="post-<?php the_ID() ?>">
           <h2 class="entry-title"><?php echo $post->post_title ?></h2>
           <div class="entry-content">
             <?php echo $post->post_content ?>
@@ -25,14 +26,13 @@
                 };
         ?>
       </div> <!-- #main-event -->
+
       <?php
         $calendar = new EC_Calendar(  );
         $calendar->displayWidget( date( 'Y' ), date( 'm' ) );
       ?>
 
       <?php include ( TEMPLATEPATH . "/LB_latest_posts.php" ); ?>
-
-    </div> <!-- .su-kien -->
 
     <?php
       if ( get_post_custom_values('comments') ) comments_template();
