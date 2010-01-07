e7f7ec3a4c416f71546cb55adf7d30944e529e9f
Gwenhael Le Moine <cycojesus@darkstar.example.net>
2010-01-07 16:44:03 +0700

    back to sandbox' default menu function
    Signed-off-by: Gwenhael Le Moine <cycojesus@darkstar.example.net>

--- header.php
+++ header.php
@@ -30,16 +30,10 @@
         </div> <!-- div#icons -->
         <div id="adress">29 Tô Hiến Thành, Hà Nội, Việt Nam</div>
       </div>
-      <div id="menu">
-        <ul>
-          <?php
-            wp_list_pages('title_li=&sort_column=menu_order&echo=1')
-          ?>
-          <li class="page_item">
-            <a href="<?php bloginfo('url') ?>">Hoạt động</a>
-          </ul>
-        </div> <!-- #menu -->
-      </div><!--  #header -->
+
+      <?php sandbox_globalnav() ?>
+
+    </div><!--  #header -->
 
       <div id="access">
         <!-- <div class="skip-link"><a href="#content" title="<?php _e( 'Skip to content', 'sandbox' ) ?>"><?php _e( 'Skip to content', 'sandbox' ) ?></a></div> -->
