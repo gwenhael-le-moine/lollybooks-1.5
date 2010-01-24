342c30846e13ed7a316b7590a42514039531a07a
Gwenhael Le moine <gwenhael.le.moine@gmail.com>
2010-01-24 22:15:16 +0700

    submisions' list on submission page
    Signed-off-by: Gwenhael Le moine <gwenhael.le.moine@gmail.com>

--- LB_contest_entry.php
+++ LB_contest_entry.php
@@ -8,10 +8,11 @@
 <div id="container" class="contest-entry">
   <div id="content">
 
-    <div id="nav-above" class="navigation">
-      <div class="nav-previous"><a href="javascript:history.back(  );">Back to Contest's page</a></div>
-    </div>
+    <!-- div id="nav-above" class="navigation">
+      <div class="nav-previous"><a href="javascript:history.back(  );">Back to main page</a></div>
+    </div -->
 
+    <div id="intro">
 
 			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
 				<h2 class="entry-title"><?php the_title() ?></h2>
@@ -26,6 +27,17 @@
 			</div><!-- .post -->
 
 <?php if ( get_post_custom_values('comments') ) comments_template() // Add a key+value of "comments" to enable comments on this page ?>
+</div>
+    <?php
+  if($post->post_parent)
+  $children = wp_list_pages("title_li=&child_of=".$post->post_parent."&echo=0");
+  else
+  $children = wp_list_pages("title_li=&child_of=".$post->ID."&echo=0");
+  if ($children) { ?>
+  <ul id="submissions">
+  <?php echo $children; ?>
+  </ul>
+  <?php } ?>
 
   </div><!-- #content -->
 </div><!-- #container -->
