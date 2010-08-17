9f4833523f94583ee1e2852864221fb0f5181037
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2010-08-17 16:38:29 +0700

    add partners' logos (requires media tags plugin) still hidden for now

--- footer.php
+++ footer.php
@@ -1,9 +1,20 @@
 
     <div id="footer">
-        <!-- div id="partners">
-        là les pubs
-        </div -->
-
+        <div id="partners" style="display:none;">
+          <!-- h1>Our partners</h1 -->
+          <div id="partners_logos">
+          <?php 
+          $media_items = get_attachments_by_media_tags( 'media_tags=partner' );
+          if ($media_items) {
+             foreach ($media_items as $media_item) {
+                echo '<a href="' . $media_item->post_excerpt . '">';
+                echo '<img src="' . wp_get_attachment_url($media_item->ID) . '" />';
+                echo '</a>';
+             }
+          }
+          ?>
+	      </div>
+        </div>
         <span id="copyright">Copyright © 2009, 2010, Lollybooks Café</span>
     </div><!-- #footer -->
 
