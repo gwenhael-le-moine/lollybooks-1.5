a09a3be493107c4f42b83de24da13cd0dfae65db
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2010-12-26 14:58:20 +0700

    indentation

--- LB_homepage_v3.php
+++ LB_homepage_v3.php
@@ -8,20 +8,21 @@
 <!--[if IE 8 ]><html <?php language_attributes(); ?> class="no-js ie ie8 lte8 lte9"><![endif]-->
 <!--[if IE 9 ]><html <?php language_attributes(); ?> class="no-js ie ie9 lte9"><![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--><html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
-	<head>
+  <head>
     <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico" />
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<title><?php
+	<title><?php
 			/*
 			 * Print the <title> tag based on what is being viewed.
 			 * We filter the output of wp_title() a bit -- see
 			 * boilerplate_filter_wp_title() in functions.php.
 			 */
 			wp_title( '|', true, 'right' );
-		?></title>
-		<link rel="profile" href="http://gmpg.org/xfn/11" />
+		      ?>
+    </title>
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); /*bloginfo( 'stylesheet_url' );*/ ?>/style-v3.css" />
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php
 		/* We add some JavaScript to pages with the comment form
 		 * to support sites with threaded comments (when in use).
@@ -36,9 +37,8 @@
 		 */
 		wp_head();
 ?>
-	</head>
-	<body <?php body_class(); ?>>
-
+  </head>
+  <body <?php body_class(); ?>>
 
     <!--banner_top-->
     <header class="banner_top">
@@ -54,7 +54,7 @@
 
     <!--menutop-->
     <nav class="menutop" role="navigation">
-           <!-- a href="#content" title="<?php esc_attr_e( 'Skip to content', 'boilerplate' ); ?>"><?php _e( 'Skip to content', 'boilerplate' ); ?></a -->
+      <!-- a href="#content" title="<?php esc_attr_e( 'Skip to content', 'boilerplate' ); ?>"><?php _e( 'Skip to content', 'boilerplate' ); ?></a -->
 	  <div class="menu-header">
         <ul id="menu-navigation" class="menu left">
           <li class="menu-item">
@@ -79,63 +79,71 @@
             <a href="#"><img src="<?php bloginfo('template_directory'); ?>/images/club.jpg" /></a>
           </li>
         </ul>
-			<!-- ?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ? -->
+		<!-- ?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ? -->
     </nav>
     <!--/menutop-->
 
-           <div class="content">
-             <div class="content_in" >
-    <aside id="contact">
-      <span class="center">
-        Số 18, Ngõ 131, Thái Hà, Hà Nội
-        ~
-        <img src="<?php bloginfo('template_directory'); ?>/images/facebook_favicon.ico" height="12px" width="12px" /> <a href="http://facebook.com/lollybooks">http://facebook.com/lollybooks</a>
-        ~
-        ✉ <a href="mailto:info@lollybooks.com">info@lollybooks.com</a>
-        ~
-        ℡ 04.6276.8545 - 0977.910.727 - 0988.172.298
-      </span>
-    </aside>
+    <div class="content">
+      <div class="content_in" >
+        <aside id="contact">
+          <span class="center">
+            Số 18, Ngõ 131, Thái Hà, Hà Nội
+            ~
+            <img src="<?php bloginfo('template_directory'); ?>/images/facebook_favicon.ico" height="12px" width="12px" /> <a href="http://facebook.com/lollybooks">http://facebook.com/lollybooks</a>
+            ~
+            ✉ <a href="mailto:info@lollybooks.com">info@lollybooks.com</a>
+            ~
+            ℡ 04.6276.8545 - 0977.910.727 - 0988.172.298
+          </span>
+        </aside>
 
-    	<aside class="left_content">
+        <?php /* END header.php */ ?>
+        <?php /* BEGIN sidebar.php */ ?>
+           
+        <aside class="left_content">
           <ul class="xoxo">
             <?php if ( ! dynamic_sidebar( 'left-widget-area-v3' ) ) :
-            endif; ?>
+                  endif; ?>
           </ul>
         </aside> <!-- left_content -->
 
         <aside class="right_content">
-			<ul class="xoxo">
-				<?php if ( ! dynamic_sidebar( 'right-widget-area-v3' ) ) :
-                endif; ?>
+		  <ul class="xoxo">
+			<?php if ( ! dynamic_sidebar( 'right-widget-area-v3' ) ) :
+                  endif; ?>
           </ul>
         </aside> <!-- right_content -->
+        
+        <?php /* END sidebar.php */ ?>
+        <?php /* BEGIN <i></i>ndex/LB_xxx.php */ ?>
 
-    <section id="content" class="middle_content" role="main">
+        <section id="content" class="middle_content" role="main">
 <?php
            query_posts( array( 'numberposts' => 1, 'tag' => 'homepage', 'orderby' => 'post_date', ) );
    while (have_posts()) : the_post();
 ?>
-<article>
-      <h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+          <article>
+            <h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
-      <div class="entry-content">
-      <?php the_content(); ?>
-      </div><!-- .entry-content -->
+            <div class="entry-content">
+              <?php the_content(); ?>
+            </div><!-- .entry-content -->
 
-      <p class="link_post_center"><a href="<?php the_permalink(); ?>">Xem chi tiết ...</a></p>
-</article>
+            <p class="link_post_center"><a href="<?php the_permalink(); ?>">Xem chi tiết ...</a></p>
+          </article>
 <?php
    endwhile;
 ?>
 		</section><!-- #main -->
+<?php /* END index/LB_xxx.php */ ?>
+<?php /* BEGIN footer.php */ ?>
 
-           </div> <!-- .content_in -->
-           </div> <!-- .content -->
+      </div> <!-- .content_in -->
+    </div> <!-- .content -->
 
-        <hr class="frise"/>
+    <hr class="frise"/>
 
-		<footer role="contentinfo">
+	<footer role="contentinfo">
 <?php
 	/* A sidebar in the footer? Yep. You can can customize
 	 * your footer with four columns of widgets.
@@ -190,13 +198,13 @@
               </div>
             </li>
           </ul>
-      	</nav>
+        </nav>
         
         <div class="text_footer">
           Copyright © 2010 <a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>. All right reserved. Địa chỉ: Số 18, ngõ 131, Thái Hà. Điện thoại: 04.6276.8545 - Email: info@lollybooks.com
         </div>
       </div>
-		</footer>
+	</footer>
 <?php
 	/* Always have wp_footer() just before the closing </body>
 	 * tag of your theme, or you will break many plugins, which
@@ -217,5 +225,6 @@ $(function() {
    $('img[usemap]').maphilight();
 });
 	</script>
-	</body>
+  </body>
 </html>
+
